0cefb871596c800d8f97f35fa7725ea94b332480
Olivier Nicole <olivier@chnik.fr>
2022-01-20 18:33:48 +0100

    Test arrays with different element types

--- src/lin_tests.ml
+++ src/lin_tests.ml
@@ -64,104 +64,118 @@ end
 module RT = Lin.Make(RConf)
 
 (*********************************************************************** *)
-(*         (Failing) tests of non-atomic int array operations            *)
+(*          (Failing) tests of non-atomic array operations               *)
 (*********************************************************************** *)
-module NAASut =
-  struct
-    let sut = Array.make 1 0
-    let get i = Array.get sut i
-    let set i v = Array.set sut i v
-    let exchange i v = let old = Array.get sut i in Array.set sut i v; old
-    let cleanup () = Array.iteri (fun i _ -> Atomic.Array.set sut i 0) sut
+
+(* Signature of array elements *)
+module type Val = sig
+  type t
+  val init : t
+  val show : t -> string
+  val gen : t Gen.t
 end
 
-module NAAConf = struct
-  type t = int array
+module Int : Val = struct
+  type t = int
+  let init = 0
+  let show = string_of_int
+  let gen = Gen.nat
+end
+
+module Float : Val = struct
+  type t = float
+  let init = 1.
+  let show = string_of_float
+  let gen = Gen.float
+end
+
+module IntOpt : Val = struct
+  type t = int option
+  let init = Some 0
+  let show =
+    let open Printf in function
+    | None -> "None"
+    | Some i -> sprintf "Some %i" i
+  let gen = Gen.opt ~ratio:0.5 Gen.nat
+end
+
+module FloatOpt : Val = struct
+  type t = float option
+  let init = Some 0.
+  let show =
+    let open Printf in function
+    | None -> "None"
+    | Some v -> sprintf "Some %f" v
+  let gen = Gen.opt ~ratio:0.5 Gen.float
+end
+
+(* Signature of array state *)
+module type ArraySut = sig
+  module Val : Val
+  type t
+  val sut8 : t
+  val get : int -> Val.t
+  val set : int -> Val.t -> unit
+  val exchange : int -> Val.t -> Val.t
+  val cleanup : unit -> unit
+end
+
+(* Non-atomic arrays *)
+module NAASut (Val : Val) = struct
+  module Val = Val
+  type t = Val.t array
+  let sut = Array.make 8 Val.init
+  let sut8 = sut
+  let get i = Array.get sut i
+  let set i v = Array.set sut i v
+  let exchange i v = let old = Array.get sut i in Array.set sut i v; old
+  let cleanup () = Array.iteri (fun i _ -> Atomic.Array.set sut i Val.init) sut
+end
+
+(* Atomic arrays *)
+module AASut (Val : Val) = struct
+  module Val = Val
+  type t = Val.t array
+  let sut = Array.make 8 Val.init
+  let sut8 = sut
+  let get i = Atomic.Array.get sut i
+  let set i v = Atomic.Array.set sut i v
+  let exchange i v = Atomic.Array.unsafe_exchange sut i v
+  let cleanup () =  Array.iteri (fun i _ -> Array.set sut i Val.init) sut
+end
+
+module ArrayConf (Val : Val) (ASut : ArraySut with module Val = Val) = struct
+  type t = ASut.t
 
   type cmd =
     | Get of int_arg
-    | Set of int_arg * int_arg
-    | Xchg of int_arg * int_arg
+    | Set of int_arg * Val.t
+    | Xchg of int_arg * Val.t
   and int_arg = int [@gen Gen.nat]
 
   let show_cmd =
     let open Printf in function
     | Get i -> sprintf "Get %i" i
-    | Set (i,v) -> sprintf "Set (%i, %i)" i v
-    | Xchg (i,v) -> sprintf "Xchg (%i, %i)" i v
+    | Set (i,v) -> sprintf "Set (%i, %s)" i (Val.show v)
+    | Xchg (i,v) -> sprintf "Xchg (%i, %s)" i (Val.show v)
 
   let gen_cmd =
     let open Gen in
     frequency
-      [1, map (fun i -> Get i) (int_bound 0);
-       1, map2 (fun i v -> Set (i,v)) (int_bound 0) nat;
-       1, map2 (fun i v -> Xchg (i,v)) (int_bound 0) nat;
+      [1, map (fun i -> Get i) (int_bound 7);
+       (*1, map2 (fun i v -> Set (i,v)) (int_bound 7) Val.gen;*)
+       1, map2 (fun i v -> Xchg (i,v)) (int_bound 7) Val.gen;
       ]
 
-  type res = RGet of int | RSet | RXchg of int
+  type res = RGet of Val.t | RSet | RXchg of Val.t
 
   let show_res =
     let open Printf in function
-    | RGet i -> sprintf "RGet %i" i
+    | RGet v -> sprintf "RGet %s" (Val.show v)
     | RSet -> sprintf "RSet"
-    | RXchg i -> sprintf "RXchg %i" i
+    | RXchg v -> sprintf "RXchg %s" (Val.show v)
 
-  let init () = NAASut.sut
-
-  let run c _r = match c with
-    | Get i -> RGet (NAASut.get i)
-    | Set (i,v) -> (NAASut.set i v; RSet)
-    | Xchg (i,v) -> (let newi = NAASut.exchange i v in RXchg newi)
-
-  let cleanup _ = NAASut.cleanup ()
-end
-
-module NAAT = Lin.Make(NAAConf);;
-
-(*********************************************************************** *)
-(*         (Failing) tests of non-atomic int array operations            *)
-(*********************************************************************** *)
-module ASut =
-  struct
-    let sut = Array.make 1 0
-    let get i = Atomic.Array.get sut i
-    let set i v = Atomic.Array.set sut i v
-    let exchange i v = Atomic.Array.exchange sut i v
-    let cleanup () = Array.iteri (fun i _ -> Atomic.Array.set sut i 0) sut
-end
-
-module AAConf = struct
-  type t = int array
-
-  type cmd =
-    | Get of int_arg
-    | Set of int_arg * int_arg
-    | Xchg of int_arg * int_arg
-  and int_arg = int [@gen Gen.nat]
-
-  let show_cmd =
-    let open Printf in function
-    | Get i -> sprintf "Get %i" i
-    | Set (i,v) -> sprintf "Set (%i, %i)" i v
-    | Xchg (i,v) -> sprintf "Xchg (%i, %i)" i v
-
-  let gen_cmd =
-    let open Gen in
-    frequency
-      [1, map (fun i -> Get i) (int_bound 0);
-       1, map2 (fun i v -> Set (i,v)) (int_bound 0) nat;
-       1, map2 (fun i v -> Xchg (i,v)) (int_bound 0) nat;
-      ]
-
-  type res = RGet of int | RSet | RXchg of int
-
-  let show_res =
-    let open Printf in function
-    | RGet i -> sprintf "RGet %i" i
-    | RSet -> sprintf "RSet"
-    | RXchg i -> sprintf "RXchg %i" i
-
-  let init () = ASut.sut
+  let init () = ASut.sut8
 
   let run c _r = match c with
     | Get i -> RGet (ASut.get i)
@@ -171,35 +185,16 @@ module AAConf = struct
   let cleanup _ = ASut.cleanup ()
 end
 
-module AAT = Lin.Make(AAConf);;
+module IntNAAT = Lin.Make(ArrayConf(Int)(NAASut(Int)));;
+module FloatNAAT = Lin.Make(ArrayConf(Float)(NAASut(Float)));;
+module IntOptNAAT = Lin.Make(ArrayConf(IntOpt)(NAASut(IntOpt)));;
+module FloatOptNAAT = Lin.Make(ArrayConf(FloatOpt)(NAASut(FloatOpt)));;
+module IntAAT = Lin.Make(ArrayConf(Int)(AASut(Int)));;
+module FloatAAT = Lin.Make(ArrayConf(Float)(AASut(Float)));;
+module IntOptAAT = Lin.Make(ArrayConf(IntOpt)(AASut(IntOpt)));;
+module FloatOptAAT = Lin.Make(ArrayConf(FloatOpt)(AASut(FloatOpt)));;
 
 (*
-(** ********************************************************************** *)
-(**                  Tests of the buggy concurrent list CList              *)
-(** ********************************************************************** *)
-module CLConf =
-struct
-  type t = CList.conc_list Atomic.t
-
-  type cmd =
-    | Add_node of int_arg
-    | Member of int_arg [@@deriving qcheck, show { with_path = false }]
-  and int_arg = int [@gen Gen.nat]
-
-  type res = RAdd_node of bool | RMember of bool [@@deriving show { with_path = false }]
-
-  let init () = CList.list_init 0
-
-  let run c r = match c with
-    | Add_node i -> RAdd_node (CList.add_node r i)
-    | Member i   -> RMember (CList.member r i)
-
-  let cleanup _ = ()
-end
-
-module CLT = Lin.Make(CLConf)
-
-
 (** ********************************************************************** *)
 (**                   Tests of the Atomic module                           *)
 (** ********************************************************************** *)
@@ -241,305 +236,22 @@ struct
 end
 
 module AT = Lin.Make(AConf)
-
-
-(** ********************************************************************** *)
-(**                      Tests of thread-unsafe [Hashtbl]                  *)
-(** ********************************************************************** *)
-module HConf =
-struct
-  type t = (char, int) Hashtbl.t
-
-  type cmd =
-    | Clear
-    | Add of char * int_arg
-    | Remove of char
-    | Find of char
-    | Find_opt of char
-    | Find_all of char
-    | Replace of char * int_arg
-    | Mem of char
-    | Length [@@deriving qcheck, show { with_path = false }]
-  and int_arg = int [@gen Gen.nat]
-
-  type res =
-    | RClear
-    | RAdd
-    | RRemove
-    | RFind of int option
-    | RFind_opt of int option
-    | RFind_all of int list
-    | RReplace
-    | RMem of bool
-    | RLength of int [@@deriving show { with_path = false }]
-
-  let init () = Hashtbl.create ~random:false 42
-
-  let run c h = match c with
-    | Clear         -> Hashtbl.clear h; RClear
-    | Add (k,v)     -> Hashtbl.add h k v; RAdd
-    | Remove k      -> Hashtbl.remove h k; RRemove
-    | Find k        -> RFind
-                         (try Some (Hashtbl.find h k)
-                          with Not_found -> None)
-    | Find_opt k    -> RFind_opt (Hashtbl.find_opt h k)
-    | Find_all k    -> RFind_all (Hashtbl.find_all h k)
-    | Replace (k,v) -> Hashtbl.replace h k v; RReplace
-    | Mem k         -> RMem (Hashtbl.mem h k)
-    | Length        -> RLength (Hashtbl.length h)
-
-  let cleanup _ = ()
-end
-
-module HT = Lin.Make(HConf)
-
-
-(** ********************************************************************** *)
-(**              Tests of [lf_list] from the lockfree library              *)
-(** ********************************************************************** *)
-module LFLConf = (* for plain list operations *)
-struct
-  type t = int Lockfree.List.t
-
-  type cmd =
-    | Push of (int [@gen Gen.nat])
-    | Pop
-    | Is_empty
-    | To_string [@@deriving qcheck, show { with_path = false }]
-
-  type res =
-    | RPush
-    | RPop of int option
-    | RIs_empty of bool
-    | RTo_string of string [@@deriving show { with_path = false }]
-
-  let init () = Lockfree.List.create ()
-
-  let run c l = match c with
-    | Push i    -> (Lockfree.List.push l i; RPush)
-    | Pop       -> RPop (Lockfree.List.pop l)
-    | Is_empty  -> RIs_empty (Lockfree.List.is_empty l)
-    | To_string -> RTo_string (Lockfree.List.to_string l string_of_int)
-
-  let cleanup _ = ()
-end
-
-module LFOLConf = (* for ordered list operations *)
-struct
-  type t = int Lockfree.List.t
-
-  type cmd =
-    | Mem of int_arg
-    | Find of int_arg
-    | SInsert of int_arg [@@deriving qcheck, show { with_path = false }]
-  and int_arg = int [@gen Gen.nat]
-
-  type res =
-    | RMem of bool
-    | RFind of int option
-    | RSInsert of bool (*sut*) [@@deriving show { with_path = false }]
-
-  let init () = Lockfree.List.create ()
-
-  let run c l = match c with
-    | Mem i     -> RMem (Lockfree.List.mem l i Int.compare)
-    | Find i    -> RFind (Lockfree.List.find l i Int.compare)
-    | SInsert i -> let b,_l' = (Lockfree.List.sinsert l i Int.compare) in
-                   RSInsert b (*FIXME l' ignored *)
-
-  let cleanup _ = ()
-end
-
-module LFLT = Lin.Make(LFLConf)
-module LFOLT = Lin.Make(LFOLConf)
-
-
-(** ********************************************************************** *)
-(**                         Tests of [lockfree.Hash]                       *)
-(** ********************************************************************** *)
-module LFHConf =
-struct
-  type t = char Lockfree.Hash.t
-
-  type cmd =
-    (*| To_string*) (* of char -> int *) (* we instead pass a meaningful element printer *)
-    | Find of int_arg
-    | Mem of int_arg
-    | Add of int_arg * char
-    | Remove of int_arg
-    | Elem_of [@@deriving qcheck, show { with_path = false }]
-  and int_arg = int [@gen Gen.nat]
-
-  type res =
-    (*| RTo_string of string*)
-    | RFind of char option
-    | RMem of bool
-    | RAdd
-    | RRemove of bool
-    | RElem_of of char list [@@deriving show { with_path = false }]
-
-  let init () = Lockfree.Hash.create ()
-
-  let run c h = match c with
-    (*| To_string -> RTo_string (Lockfree.Hash.to_string h (String.make 1))*)
-    | Find k    -> RFind (Lockfree.Hash.find h k)
-    | Mem k     -> RMem (Lockfree.Hash.mem h k)
-    | Add (k,v) -> Lockfree.Hash.add h k v; RAdd
-    | Remove k  -> RRemove (Lockfree.Hash.remove h k)
-    | Elem_of   -> RElem_of (Lockfree.Hash.elem_of h)
-
-  let cleanup _ = ()
-end
-
-module LFHT = Lin.Make(LFHConf)
-
-
-(** ********************************************************************** *)
-(**                         Tests of [lockfree.Bag]                        *)
-(** ********************************************************************** *)
-module LFBConf =
-struct
-  type t = int Lockfree.Bag.t
-
-  type cmd =
-    | Is_empty
-    | Push of (int [@gen Gen.nat])
-    | Pop [@@deriving qcheck, show { with_path = false }]
-
-  type res =
-    | RIs_empty of bool
-    | RPush
-    | RPop of int option [@@deriving show { with_path = false }]
-
-  let init () = Lockfree.Bag.create ()
-
-  let run c h = match c with
-    | Is_empty -> RIs_empty (Lockfree.Bag.is_empty h)
-    | Push e   -> Lockfree.Bag.push h e; RPush
-    | Pop      -> RPop (Lockfree.Bag.pop h)
-
-  let cleanup _ = ()
-end
-
-module LFBT = Lin.Make(LFBConf)
-
-
-(** ********************************************************************** *)
-(**                             Tests of [Kcas]                            *)
-(** ********************************************************************** *)
-module KConf =
-struct
-  type t = int Kcas.ref
-
-  (* missing: equal, is_on_ref, kCAS -- mk_cas, commit (already tested as [set] *)
-  type cmd =
-    | Get
-    | Set of int_arg
-    | Cas of int_arg * int_arg
-    | TryMapNone
-    (*| TryMapSome*) (* Seq,exec cannot fail - hence not linearizable with [try_map] *)
-    | MapNone
-    | MapSome
-    | Incr
-    | Decr [@@deriving qcheck, show { with_path = false }]
-  and int_arg = (int [@gen Gen.nat])
-
-
-  type res =
-    | RGet of int
-    | RSet
-    | RCas of bool
-    | RTryMapNone of int cas_result
-    (*| RTryMapSome of int cas_result*)
-    | RMapNone of int cas_result
-    | RMapSome of int cas_result
-    | RIncr
-    | RDecr [@@deriving show { with_path = false }]
-  and 'a cas_result
-    = 'a Kcas.cas_result =
-    | Aborted
-    | Failed
-    | Success of 'a [@deriving show { with_path = false }]
-
-  let init () = Kcas.ref 0
-
-  let run c r = match c with
-    | Get        -> RGet (Kcas.get r)
-    | Set i      -> (Kcas.set r i; RSet)
-    | Cas (i,j)  -> RCas (Kcas.cas r i j)
-    | TryMapNone -> RTryMapNone (Kcas.try_map r (fun _ -> None))
-    (*| TryMapSome -> RTryMapSome (Kcas.try_map r (fun i -> Some i))*)
-    | MapNone    -> RMapNone (Kcas.map r (fun _ -> None))
-    | MapSome    -> RMapSome (Kcas.map r (fun i -> Some i))
-    | Incr       -> (Kcas.incr r; RIncr)
-    | Decr       -> (Kcas.decr r; RDecr)
-  let cleanup _ = ()
-end
-
-module KT = Lin.Make(KConf)
-
-
-(** ********************************************************************** *)
-(**                           Tests of [Kcas.W1]                           *)
-(** ********************************************************************** *)
-module KW1Conf =
-struct
-  type t = int Kcas.W1.ref
-
-  type cmd =
-    | Get
-    | Set of int_arg
-    | Cas of int_arg * int_arg
-    | TryMapNone
-    (*| TryMapSome*) (* Seq,exec cannot fail - hence not linearizable with [try_map] *)
-    | MapNone
-    | MapSome
-    | Incr
-    | Decr [@@deriving qcheck, show { with_path = false }]
-  and int_arg = (int [@gen Gen.nat])
-
-
-  type res =
-    | RGet of int
-    | RSet
-    | RCas of bool
-    | RTryMapNone of int cas_result
-    (*| RTryMapSome of int cas_result*)
-    | RMapNone of int cas_result
-    | RMapSome of int cas_result
-    | RIncr
-    | RDecr [@@deriving show { with_path = false }]
-  and 'a cas_result
-    = 'a Kcas.cas_result =
-    | Aborted
-    | Failed
-    | Success of 'a [@deriving show { with_path = false }]
-
-  let init () = Kcas.W1.ref 0
-
-  let run c r = match c with
-    | Get        -> RGet (Kcas.W1.get r)
-    | Set i      -> (Kcas.W1.set r i; RSet)
-    | Cas (i,j)  -> RCas (Kcas.W1.cas r i j)
-    | TryMapNone -> RTryMapNone (Kcas.W1.try_map r (fun _ -> None))
-    (*| TryMapSome -> RTryMapSome (Kcas.W1.try_map r (fun i -> Some i))*)
-    | MapNone    -> RMapNone (Kcas.W1.map r (fun _ -> None))
-    | MapSome    -> RMapSome (Kcas.W1.map r (fun i -> Some i))
-    | Incr       -> (Kcas.W1.incr r; RIncr)
-    | Decr       -> (Kcas.W1.decr r; RDecr)
-  let cleanup _ = ()
-end
-
-module KW1T = Lin.Make(KW1Conf)
-;;
 *)
 
 Util.set_ci_printing ()
 ;;
 QCheck_runner.run_tests_main [
-  NAAT.lin_test    ~count:10000 ~name:"non-atomic array test";
-  AAT.lin_test    ~count:10000 ~name:"Atomic.Array test";
   (*
+  IntNAAT.lin_test    ~count:1000 ~name:"non-atomic int array test";
+  IntAAT.lin_test    ~count:1000 ~name:"atomic int array test";
+  *)
+  FloatAAT.lin_test    ~count:1000 ~name:"atomic float array test";
+  FloatNAAT.lin_test    ~count:1000 ~name:"non-atomic float array test";
+  (*
+  IntOptNAAT.lin_test    ~count:1000 ~name:"non-atomic int option array test";
+  IntOptAAT.lin_test    ~count:1000 ~name:"atomic int option array test";
+  FloatOptNAAT.lin_test    ~count:1000 ~name:"non-atomic float option array test";
+  FloatOptAAT.lin_test    ~count:1000 ~name:"atomic float option array test";
   RT.lin_test     ~count:1000 ~name:"ref test";
   CLT.lin_test    ~count:1000 ~name:"CList test";
   AT.lin_test     ~count:1000 ~name:"Atomic test";
